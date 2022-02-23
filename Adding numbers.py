c0631c638630c9e99a11a04ddb9c2312c93ce8dc
STyler4 <98915469+STyler4@users.noreply.github.com>
2022-02-24 09:35:24 +1300

    update

---
+++ Adding numbers.py
@@ -0,0 +1,15 @@
+def calculate_addition(num_1):
+    addition = num_1
+    return addition
+
+
+def calculate_addition2(num_2):
+    addition2 = num_2
+    return addition2
+
+
+# Main
+question = int(input("Enter first number: "))
+question1 = int(input("Enter first number: "))
+answer = calculate_addition(question + question1)
+print()
