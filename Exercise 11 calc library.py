34b0f6a48ff8ef0fa4e5c1927cad6a9575864db1
STyler4 <98915469+STyler4@users.noreply.github.com>
2022-03-03 09:22:13 +1300

    update

---
+++ Exercise 11 calc library.py
@@ -0,0 +1,17 @@
+def overdue_fines(days_overdue):
+    base_charge = .5
+    daily_charge = .8
+    max_charge = 30
+    gross_charge = base_charge + (days_overdue * daily_charge)
+    if gross_charge > max_charge:
+        fine = max_charge
+    else:
+        fine = gross_charge
+    return fine
+
+
+# Main routine
+days_overdue_ = int(input("Enter days overdue: "))
+print(f"For {days_overdue_} days overdue the fine is ${overdue_fines(days_overdue_):.2f}")
+
+
---
+++ Exercise 8 print word.py
@@ -0,0 +1,10 @@
+def print_word(word, number):
+    word1 = word[0:number]
+    word2 = word[number:len(word)]
+    print(f"{word1.upper()}{word2}")
+    
+
+# Main routine 
+word_ = input("Enter word: ")
+number_ = int(input("Number of chars to capitalise: "))
+print_word(word_, number_)
